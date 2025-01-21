1ac1362e6fb41ed78e6d1d419c61d3ae038e5a8a
pri3197 <priyankagala3197@gmail.com>
2025-01-21 20:12:27 +0100

    Fix UI

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,29 +1,28 @@
 import React from "react";
 
 import "../style/About.css";
+import background_resume from '../media/background_Resume.png';
 function About() {
   return (
     <div className="about">
       <div
         className="aboutTop"
-  
-      > </div>
+  style={{backgroundImage:`url(${background_resume})`}}> </div>
       <div className="aboutBottom">
-      <h1> ABOUT US</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
+      <h1> ABOUT ME</h1>
+        <p style={{textAlign:"center"}}>
+        Software Engineer having more than 5 years of IT experience. 
+        Hands-on experience Springboot, Maven, and Restful Web Services technologies like SOAP and Restful Web Services. Having a knowledge of Java, Spring Framework, ReactJs, HTML, CSS3, & Javascript (ES6) in addition to having basics in Hibernate and Oracle database concepts.
+        I have completed my Masters Thesis in Ethical Compliance in Smart City data platforms. 
+        <br/>Sprachen: Hindi, Englisch (C1), Deutsch (B2), Französisch (A1)  <br/>
+        Programmiersprachen: Java, Python, ReactJs, XML, HTML, CSS  <br/>
+        Datenbanken: SQL, Postgres, Oracle  <br/>
+        Frameworks: Spring MVC, Spring Boot, Hibernate, JPA  <br/>
+        Versionskontrolle: Git, Maven, Gitlab  <br/>
+        Operating System: Windows, Linux  <br/>
+        Container/Cloud: Docker, AWS  <br/>
+       
+       
         </p>
       </div>
     </div>
