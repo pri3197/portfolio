23e8fa45200a570acbdbdcbd1c3139616364ca09
pri3197 <priyankagala3197@gmail.com>
2025-01-21 13:51:22 +0100

    Initial commit

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,15 @@
+import Backend from "../media/Backend.png";
+import GeoFence from '../media/GeoFence.png'
+
+export const ProjectList = [
+  {
+    name: "Standardisation",
+    image: Backend,
+    link: "https://www.mein-check-in.de/kaiserslautern/stellenangebote"
+  },
+  {
+    name:'Geofencing- In development',
+    image:GeoFence,
+    link:''
+  }
+ ];
